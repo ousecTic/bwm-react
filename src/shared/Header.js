5e263a9afc777674c5a63c306a810b0de49a5711
Henry Ly <henryly213@gmail.com>
2019-08-12 18:47:10 -0500

    initial commit

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,5 +1,48 @@
 import React from "react";
 
 export function Header() {
-  return <h1>I am Header Component</h1>;
+  return (
+    <nav classname="navbar navbar-dark navbar-expand-lg">
+      <div classname="container">
+        <a classname="navbar-brand" href>
+          BookWithMe
+        </a>
+        <form classname="form-inline my-2 my-lg-0">
+          <input
+            classname="form-control mr-sm-2 bwm-search"
+            type="search"
+            placeholder="Try ' new york'"
+            aria-label="Search"
+          />
+          <button
+            classname="btn btn-outline-success my-2 my-sm-0 btn-bwm-search"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+        <button
+          classname="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span classname="navbar-toggler-icon" />
+        </button>
+        <div classname="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div classname="navbar-nav ml-auto">
+            <a classname="nav-item nav-link active" href>
+              Login <span classname="sr-only">(current)</span>
+            </a>
+            <a classname="nav-item nav-link" href>
+              Register
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
